e3a4c89814f103c03d502d16dfa0629042efc475
Gabriel Adomnicai <gabesoft@gmail.com>
2013-11-26 08:11:14 -0800

    more fns

--- base.scm
+++ base.scm
@@ -33,7 +33,7 @@
       (else (cons (car (car l))
                   (firsts (cdr l)))))))
 
-; insert new to the right of old in l
+; insert new to the right of the first occurrence of old in l
 (define insertR
   (lambda (new old l)
     (cond
@@ -42,3 +42,39 @@
                                (cons new (cdr l))))
       (else (cons (car l)
                   (insertR new old (cdr l)))))))
+
+; insert new to the left of the first occurrence of old in l
+(define insertL
+  (lambda (new old l)
+    (cond
+      ((null? l) l)
+      ((eq? (car l) old) (cons new l))
+      (else (cons (car l)
+                  (insertL new old (cdr l)))))))
+
+; replace the first occurrence old with new in l
+(define subst
+  (lambda (new old l)
+    (cond
+      ((null? l) l)
+      ((eq? (car l) old) (cons new (cdr l)))
+      (else (cons (car l)
+                  (subst new old (cdr l)))))))
+
+; replace the first occurrence of o1 or o2 with new in l
+(define subst2
+  (lambda (new o1 o2 l)
+    (cond
+      ((null? l) l)
+      ((or (eq? (car l) o1)
+           (eq? (car l) o2)) (cons new (cdr l)))
+      (else (cons (car l)
+                  (subst2 new o1 o2 (cdr l)))))))
+
+; remove a member (all occurrences)
+(define mrmember
+  (lambda (a lat)
+    (cond
+      ((null? lat) lat)
+      ((eq? (car lat) a) (mrmember a (cdr lat)))
+      (else (cons (car lat) (mrmember a (cdr lat)))))))
